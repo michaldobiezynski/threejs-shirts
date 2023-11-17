dc4d24fb228deba4830683cb03fc65fcd7cb6c91
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-17 06:33:35 +0000

    add imports, props, styling and functionality to CustomButton

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,5 +1,25 @@
-const CustomButton = () => {
-  return <div>CustomButton</div>;
+import { useSnapshot } from "valtio";
+import state from "../store";
+
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
+  const snap = useSnapshot(state);
+
+  const generateStyle = (type) => {
+    if (type === "filled")
+      return {
+        backgroundColor: snap.color,
+        color: "#fff",
+      };
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}>
+      {title}
+    </button>
+  );
 };
 
 export default CustomButton;
