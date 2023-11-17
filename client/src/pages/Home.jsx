c4c8c9edf2c9adceaba93424442784f6f8890064
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-17 06:33:50 +0000

    fix custom button import

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 
-import CustomButton from "../components";
+import { CustomButton } from "../components";
 
 import state from "../store";
 
