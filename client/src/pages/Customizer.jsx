cc390387fc98f4a0896ec56d6c59f77debc0fe66
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-17 06:40:02 +0000

    add imports to Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,3 +1,14 @@
+import { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import config from "../config";
+import state from "../store";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, sliderAnimation } from "../config/motion";
+
 const Customizer = () => {
   return <div>Customizer</div>;
 };
