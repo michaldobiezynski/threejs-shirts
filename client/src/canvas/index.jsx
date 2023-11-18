688effaa7e8efc402238d6b9616d3cd157e45886
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-18 15:43:32 +0000

    rename js tpo jsx

---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,8 @@
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+
+const CanvasModel = () => {
+  return <div>Canvas</div>;
+};
+
+export default CanvasModel;
