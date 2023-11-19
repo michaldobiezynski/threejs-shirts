36fa522cc09f6bcf3baf2dcec7fa5ecc4fdb89e1
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-19 19:02:10 +0000

    style and add content to the Tab component

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { useSnapshot } from "valtio";
 
-const Tab = () => {
-  return <div>Tab</div>;
+import state from "../store";
+
+const Tab = ({ tab, isFilteredTab, isActiveTab, handleClick }) => {
+  const snap = useSnapshot(state);
+
+  const activeStyles =
+    isFilteredTab && isActiveTab
+      ? { backgroundColor: snap.color, opacity: 0.5 }
+      : { backgroundColor: "transparent", opacity: 1 };
+
+  return (
+    <div
+      onClick={handleClick}
+      key={tab.name}
+      className={`tab ${
+        isFilteredTab ? "rounded-full glassmorphism" : "rounded-4"
+      }`}
+      style={activeStyles}>
+      <img src={tab.icon} alt={tab.name} />
+    </div>
+  );
 };
 
 export default Tab;
